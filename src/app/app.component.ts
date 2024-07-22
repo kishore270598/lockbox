c63513e03e1b9b8da81d2a922a060b6ef0d7e750
kishore270598 <kk94323@gmail.com>
2024-07-23 01:49:46 +0530

    ngForm Added for the site component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SiteListComponent } from './site-list/site-list.component';
-
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { NgModule } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, SiteListComponent],
+  imports: [RouterOutlet, NavbarComponent, SiteListComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/site-list/site-list.component.ts
+++ src/app/site-list/site-list.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-site-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './site-list.component.html',
-  styleUrl: './site-list.component.css'
+  styleUrl: './site-list.component.css',
 })
 export class SiteListComponent {
-
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+  }
 }
