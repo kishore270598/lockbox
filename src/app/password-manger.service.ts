e9a2be1862a58df36e4a53b8478e708d3973e8c6
kishore270598 <kk94323@gmail.com>
2024-07-25 02:30:20 +0530

    DATABASE FIRST push

--- src/app/password-manger.service.ts
+++ src/app/password-manger.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {
+  Firestore,
+  collection,
+  addDoc,
+  collectionData,
+} from '@angular/fire/firestore';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PasswordMangerService {
-
-  constructor() { }
+  constructor(private firestore: Firestore) {}
+  addSite(data: object) {
+    const dbInstance = collection(this.firestore, 'sites');
+    return addDoc(dbInstance, data);
+  }
+  //IDfield is optional but if we don't give id it won't display
+  loadSites() {
+    const dbInstance = collection(this.firestore, 'sites');
+    return collectionData(dbInstance, { idField: 'id' });
+  }
 }
--- src/app/site-list/site-list.component.ts
+++ src/app/site-list/site-list.component.ts
@@ -1,16 +1,39 @@
+import { PasswordMangerService } from './../password-manger.service';
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgModel } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-site-list',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule, NgFor],
   templateUrl: './site-list.component.html',
   styleUrl: './site-list.component.css',
 })
 export class SiteListComponent {
-  onSubmit(form: NgForm) {
-    console.log(form.value);
+  allSites!: Observable<Array<any>>;
+  constructor(private PasswordMangerService: PasswordMangerService) {
+    this.loadSites();
+  }
+
+  //Once the submit is clicked it will call the service ,for that we pass the values.
+  //when its correct it will tell data save
+  //else error message
+
+  onSubmit(values: object) {
+    console.log(values);
+    this.PasswordMangerService.addSite(values)
+      .then(() => {
+        console.log('Data save success');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  loadSites() {
+    this.allSites = this.PasswordMangerService.loadSites();
   }
 }
