ef71712187ec9a1f87f1d8c953aa9f745a3773c1
kishore270598 <kk94323@gmail.com>
2024-08-04 04:14:36 +0530

    EDIT button and add button in password list component

--- src/app/password-list/password-list.component.ts
+++ src/app/password-list/password-list.component.ts
@@ -1,12 +1,89 @@
+import { PasswordMangerService } from './../password-manger.service';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-password-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './password-list.component.html',
-  styleUrl: './password-list.component.css'
+  styleUrl: './password-list.component.css',
 })
+//Activated route act as a observer so we need a subscribe
 export class PasswordListComponent {
+  psiteId!: string;
+  psiteName!: string;
+  psiteURL!: string;
+  psiteImgURl!: string;
+  passwordList!: Observable<Array<any>>;
+  email!: string;
+  username!: string;
+  password!: string;
+  passwordId!: string;
+  formState: string = 'Add New';
+  constructor(
+    private route: ActivatedRoute,
+    private PasswordMangerService: PasswordMangerService
+  ) {
+    this.route.queryParams.subscribe((val: any) => {
+      this.psiteId = val.id;
+      this.psiteName = val.siteName;
+      this.psiteImgURl = val.siteImageUrl;
+      this.psiteURL = val.siteUrl;
+    });
+    this.loadPassword();
+  }
+  restForm() {
+    this.email = '';
+    this.username = '';
+    this.password = '';
+    this.formState = 'Add new';
+    this.passwordId = '';
+  }
 
+  onSubmit(value: object) {
+    if (this.formState == 'Add New') {
+      this.PasswordMangerService.addPassword(value, this.psiteId)
+        .then(() => {
+          console.log('Password saved successfully ');
+          //to reset the page
+          this.restForm();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else if (this.formState == 'Edit') {
+      this.PasswordMangerService.updatePassword(
+        this.psiteId,
+        this.passwordId,
+        value
+      )
+        .then(() => {
+          console.log('Password updated successfully ');
+          //to reset the page
+          this.restForm();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
+  loadPassword() {
+    this.passwordList = this.PasswordMangerService.loadPasswords(this.psiteId);
+  }
+  editPassword(
+    email: string,
+    username: string,
+    password: string,
+    passwordId: string
+  ) {
+    this.email = email;
+    this.username = username;
+    this.password = password;
+    this.passwordId = passwordId;
+    this.formState = 'Edit';
+  }
 }
--- src/app/password-manger.service.ts
+++ src/app/password-manger.service.ts
@@ -31,4 +31,22 @@ export class PasswordMangerService {
     const docInstance = doc(this.firestore, 'sites', id);
     return deleteDoc(docInstance);
   }
+
+  //password list queries
+  addPassword(data: object, siteId: string) {
+    const dbInstance = collection(this.firestore, `sites/${siteId}/passwords`);
+    return addDoc(dbInstance, data);
+  }
+  loadPasswords(siteId: string) {
+    const dbInstance = collection(this.firestore, `sites/${siteId}/passwords`);
+    return collectionData(dbInstance, { idField: 'id' });
+  }
+  updatePassword(siteId: string, passwordId: string, data: object) {
+    const docInstance = doc(
+      this.firestore,
+      `sites/${siteId}/passwords`,
+      passwordId
+    );
+    return updateDoc(docInstance, data);
+  }
 }
